fcdffee1bacd904c5816bf9b82d9a0bf03eb23eb
dotupNET <dotup.software@gmail.com>
2020-10-26 15:57:37 +0100

    Database exported

--- sql-scripts/MySqlSync.sql
+++ sql-scripts/MySqlSync.sql
@@ -305,25 +305,12 @@ CREATE TABLE `SyncProtocolClients` (
   `SyncTo` timestamp(3) NULL DEFAULT NULL,
   `SyncId` char(36) DEFAULT NULL,
   `SyncResponse` json DEFAULT NULL,
-  `CreateTime` timestamp(3) NULL DEFAULT current_timestamp(3),
+  `CreateTime` timestamp(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
   `UpdateTime` timestamp(3) NULL DEFAULT NULL,
   `DeleteTime` timestamp(3) NULL DEFAULT NULL,
   PRIMARY KEY (`RowId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `SyncSystemLog`
@@ -1064,4 +1051,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-26 14:48:07
+-- Dump completed on 2020-10-26 15:55:35
